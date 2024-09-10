37e115db302a4a41c661f1285da1b2d5b0579562
kevinccbsg <kevinccbsg@gmail.com>
2024-09-10 22:45:10 +0200

    feat add csv example

--- examples/09_destructuring.js
+++ examples/09_destructuring.js
@@ -1,3 +1,58 @@
 console.log('09');
 
+const twilight = { title: 'Twilight', author: 'Stephenie Meyer', year: 2005 };
+const { title } = { title: 'Twilight', author: 'Stephenie Meyer', year: 2005 };
 
+console.log(twilight.title);
+console.log(title);
+
+const books = [
+  { title: '1984', author: 'George Orwell', year: 1949 },
+  {
+    title: 'One Hundred Years of Solitude',
+    author: 'Gabriel García Márquez',
+    year: 1967,
+  },
+  {
+    title: "Harry Potter and the Philosopher's Stone",
+    author: 'J.K. Rowling',
+    year: 1997,
+  },
+  { title: 'The Da Vinci Code', author: 'Dan Brown', year: 2003 },
+  { title: 'Twilight', author: 'Stephenie Meyer', year: 2005 },
+  { title: 'The Hunger Games', author: 'Suzanne Collins', year: 2008 },
+];
+
+
+const getTheTittle = (books) =>{
+  let result = [];
+  for (let i = 0; i < books.length; i++) {
+    const { year, title } = books[i];
+    if (year > 2000) {
+      console.log(title);
+    }
+  }
+  return result;
+};
+
+const csvProducts = 'T-shirt,10,Amazon\nCap,5,Google\nPants,15,Amazon\nShoes,20,Google';
+
+
+
+const processCSV = (csv) => {
+  const products = [];
+  const lines = csv.split('\n'); // ['T-shirt,10,Amazon', 'Cap,5,Google', 'Pants,15,Amazon','Shoes,20,Google']
+  for (let line of lines) {
+    // line 'T-shirt,10,Amazon'
+    const [name, price, store] = line.split(','); // ['T-shirt', '10', 'Amazon']
+    products.push({
+      name: name,
+      price: parseInt(price),
+      store: store
+    });
+  }
+  return products;
+};
+
+const products = processCSV(csvProducts);
+console.log(products);
